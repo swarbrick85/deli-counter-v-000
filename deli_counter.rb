e09de47b561a6e034c0d5ba037d4325bc9aaf105
David F Swarbrick Jr <david.swarbrick.jr@gmail.com>
2018-10-09 20:38:30 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -12,5 +12,5 @@ def line(katz_deli)
 end
 
 def take_a_number(katz_deli, name)
-  puts "#{katz_deli.size + 1}. #{name}"
+  puts "Welcome #{name}. You are number #{katz_deli.size + 1} in line. "
 end 
