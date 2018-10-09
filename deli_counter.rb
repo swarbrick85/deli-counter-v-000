c4d010a26ef6e6905f3e5907f4639d40a3c60b19
David F Swarbrick Jr <david.swarbrick.jr@gmail.com>
2018-10-09 20:32:33 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,13 @@
-# Write your code here.
+def line(katz_deli)
+  if katz_deli.size == 0 
+    puts "The line is currently empty."
+  else
+    x = 0
+    while x < katz_deli.size 
+    katz_deli[x] = " #{x + 1}. #{katz_deli[x]}"
+    x += 1 
+    end
+    puts "The line is currently:#{katz_deli.join}"  
+  end
+end
+
